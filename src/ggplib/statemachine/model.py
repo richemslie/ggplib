17b85b0d7a783d519806d58a2c2b81b591d22b6f
Richard Emslie <richardemslie@gmail.com>
2017-11-29 17:22:24 +0000

    statemachine model

---
+++ src/ggplib/statemachine/model.py
@@ -0,0 +1,37 @@
+from collections import OrderedDict
+
+class StateMachineModel(object):
+    def __init__(self):
+        # will populate later
+        self.roles = []
+        self.bases = []
+        self.actions = []
+
+    def get_roles(self):
+        return self.roles
+
+    def to_description(self):
+        d = OrderedDict()
+        d['roles'] = self.roles
+        d['bases'] = self.bases
+        d['actions'] = self.actions
+        return d
+
+    def from_description(self, info):
+        self.roles = info["roles"]
+        self.bases = info["bases"]
+        self.actions = info["actions"]
+
+    def from_propnet(self, propnet):
+        self.roles = [ri.role for ri in propnet.role_infos]
+        self.bases = []
+        self.actions = [[] for ri in propnet.role_infos]
+
+        for b in propnet.base_propositions:
+            self.bases.append(str(b.meta.gdl))
+
+        for ri in propnet.role_infos:
+            actions = self.actions[ri.role_index]
+
+            for a in ri.inputs:
+                actions.append(str(a.meta.gdl))
