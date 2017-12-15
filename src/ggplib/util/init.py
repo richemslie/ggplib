22e2ac643285cfa6502b3c55c6ba917fb127408a
Richard Emslie <richardemslie@gmail.com>
2017-12-15 04:42:23 +0000

    dont call init() twice

--- src/ggplib/util/init.py
+++ src/ggplib/util/init.py
@@ -8,6 +8,4 @@ def setup_once(log_name_base="test"):
     global _setup_once
     if not _setup_once:
         interface.initialise_k273(1, log_name_base=log_name_base)
-
-        interface.initialise_k273(1)
         ggplib.util.log.initialise()
